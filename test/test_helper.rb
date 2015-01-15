5139d7379054bc74476c328d400c318e7b96ff42
Mark Oude Veldhuis <mark.oudeveldhuis@nedap.com>
2015-01-15 09:30:47 +0100

    Configure Git directory as '.' and allow connections to CodeClimate
    Thx @antek-drzewiecki

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+CodeClimate::TestReporter.configuration.git_dir = "."
 
 require_relative '../lib/mandrill-delivery-method'
 require 'rails/all'
@@ -9,4 +10,4 @@ require 'minitest/mock'
 require 'webmock'
 require 'webmock/minitest'
 
-WebMock.disable_net_connect!
+WebMock.disable_net_connect! allow: 'codeclimate.com'
